5772e8cf716c34c3258e2d9d3b801f882ee7809f
Sunc <363587351@qq.com>
2024-06-20 09:54:50 +0800

    Update index.js
    修改成自己域名

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.sun363587351.xyz": dockerHub,
+  "quay.sun363587351.xyz": "https://quay.io",
+  "gcr.sun363587351.xyz": "https://gcr.io",
+  "k8s-gcr.sun363587351.xyz": "https://k8s.gcr.io",
+  "k8s.sun363587351.xyz": "https://registry.k8s.io",
+  "ghcr.sun363587351.xyz": "https://ghcr.io",
+  "cloudsmith.sun363587351.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.sun363587351.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
